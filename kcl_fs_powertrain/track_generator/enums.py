bab171c2849fac211c23dd49efa16a3cb76e23bf
HiromoriOkushi <ejavid2018@gmail.com>
2025-03-24 01:57:17 +0000

    UPDATE

--- kcl_fs_powertrain/track_generator/enums.py
+++ kcl_fs_powertrain/track_generator/enums.py
@@ -4,12 +4,12 @@ from enum import Enum
 
 class TrackMode(Enum):
     """Possible modes for how Voronoi regions are selected"""
-    EXPAND = "expand"   # Results in roundish track shapes
-    EXTEND = "extend"   # Results in elongated track shapes
-    RANDOM = "random"   # Select regions randomly
+    EXPAND = 1  # Results in roundish track shapes
+    EXTEND = 2   # Results in elongated track shapes
+    RANDOM = 3  # Select regions randomly
 
 class SimType(Enum):
     """Selection between output format for different simulators"""
-    FSSIM = "fssim"     # FSSIM compatible .yaml file
-    FSDS = "fsds"       # FSDS compatible .csv file
-    GPX = "gpx"         # GPX track format
+    FSSIM = 1    # FSSIM compatible .yaml file
+    FSDS = 2       # FSDS compatible .csv file
+    GPX = 3         # GPX track format
--- kcl_fs_powertrain/track_generator/generator.py
+++ kcl_fs_powertrain/track_generator/generator.py
@@ -13,17 +13,19 @@ import gpxpy.gpx
 import math
 from typing import List, Dict, Tuple, Optional
 
+from enum import Enum
+
 class TrackMode(Enum):
     """Possible modes for how Voronoi regions are selected"""
-    EXPAND = "expand"   # Results in roundish track shapes
-    EXTEND = "extend"   # Results in elongated track shapes
-    RANDOM = "random"   # Select regions randomly
+    EXPAND = 1   # Results in roundish track shapes
+    EXTEND = 2   # Results in elongated track shapes
+    RANDOM = 3   # Select regions randomly
 
 class SimType(Enum):
     """Selection between output format for different simulators"""
-    FSSIM = "fssim"     # FSSIM compatible .yaml file
-    FSDS = "fsds"       # FSDS compatible .csv file
-    GPX = "gpx"         # GPX track format
+    FSSIM = 1     # FSSIM compatible .yaml file
+    FSDS = 2       # FSDS compatible .csv file
+    GPX = 3         # GPX track format
 
 class FSTrackGenerator:
     def __init__(self, base_dir: str, visualize: bool = False):
